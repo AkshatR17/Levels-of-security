45cb08277147d75c094a0175991f299ea1b978d7
Akshat Garg <akshatgarg.cricketer@gmail.com>
2024-01-04 18:39:06 +0530

    Add environment variables

--- app.js
+++ app.js
@@ -1,4 +1,20 @@
 //jshint esversion:6
+require('dotenv').config()
+// console.log(process.env) 
+
+/*
+Certainly, you can create a file like secrets.js to store sensitive information and add it to .gitignore to prevent it from being committed to version control. This approach can work, but there are some differences between using a dedicated library like dotenv and creating your own configuration file:
+
+Convention and Standardization: dotenv follows a widely adopted convention of using a file named .env to store environment variables. This convention is recognized by many developers and tools, making it easier for others to understand and contribute to your project. Creating a custom file like secrets.js may be less standardized and might not be immediately clear to someone else working on your project.
+
+Parsing and Loading: The dotenv module is specifically designed to parse a .env file and load its contents into the process.env object. It handles various data types and formats, making it convenient for managing environment variables. If you create your own file, you'll need to handle the parsing and loading of variables yourself.
+
+Ease of Use: Using dotenv is straightforward and requires minimal code. You only need to call require('dotenv').config() to load the variables from the .env file. Custom solutions may involve more code and complexity.
+
+Community Support: dotenv is a widely used and well-maintained library with community support. It's actively developed and tested, ensuring that it works seamlessly with different Node.js projects. If you create your own solution, you'll need to maintain and test it yourself.
+
+Integration with Deployment Tools: Many deployment and hosting platforms provide native support for loading environment variables from a .env file. Using dotenv can make it easier to integrate your Node.js application with such platforms.
+*/
 
 const express = require('express');
 const ejs = require('ejs');
@@ -21,8 +37,7 @@ const userSchema = new mongoose.Schema({
     password: String
 });
 
-const secret = "ThisIsLevel2OfAuthentication.";
-userSchema.plugin(encrypt, {secret, encryptedFields: ['password']});
+userSchema.plugin(encrypt, {secret: process.env.SECRET, encryptedFields: ['password']});
 
 const user = mongoose.model('userData', userSchema);
 
