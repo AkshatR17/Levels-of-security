f180e59c241955f7a82934bb1132acbcd5b349be
Akshat Garg <akshatgarg.cricketer@gmail.com>
2024-01-05 23:15:02 +0530

    Level 5: Cookies and sessions

--- app.js
+++ app.js
@@ -2,28 +2,18 @@
 require('dotenv').config()
 // console.log(process.env) 
 
-/*
-Certainly, you can create a file like secrets.js to store sensitive information and add it to .gitignore to prevent it from being committed to version control. This approach can work, but there are some differences between using a dedicated library like dotenv and creating your own configuration file:
-
-Convention and Standardization: dotenv follows a widely adopted convention of using a file named .env to store environment variables. This convention is recognized by many developers and tools, making it easier for others to understand and contribute to your project. Creating a custom file like secrets.js may be less standardized and might not be immediately clear to someone else working on your project.
-
-Parsing and Loading: The dotenv module is specifically designed to parse a .env file and load its contents into the process.env object. It handles various data types and formats, making it convenient for managing environment variables. If you create your own file, you'll need to handle the parsing and loading of variables yourself.
-
-Ease of Use: Using dotenv is straightforward and requires minimal code. You only need to call require('dotenv').config() to load the variables from the .env file. Custom solutions may involve more code and complexity.
-
-Community Support: dotenv is a widely used and well-maintained library with community support. It's actively developed and tested, ensuring that it works seamlessly with different Node.js projects. If you create your own solution, you'll need to maintain and test it yourself.
-
-Integration with Deployment Tools: Many deployment and hosting platforms provide native support for loading environment variables from a .env file. Using dotenv can make it easier to integrate your Node.js application with such platforms.
-*/
-
 const express = require('express');
 const ejs = require('ejs');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 // const encrypt = require('mongoose-encryption');
 // const md5 = require('md5');
-const bcrypt = require('bcrypt');
-const saltRounds = 10;
+// const bcrypt = require('bcrypt');
+// const saltRounds = 10;
+const session = require('express-session');
+const passport = require('passport');
+const passportLocalMongoose = require('passport-local-mongoose');
+// we don't need to require passport local as passport local mongoose will explicitly require it.
 
 const app = express();
 
@@ -33,18 +23,39 @@ app.use(bodyParser.urlencoded({
     extended: true
 }));
 
-mongoose.connect('mongodb+srv://admin:H8gVgrtTkTOAT27A@cluster0.zirtemd.mongodb.net/userData');
+
+app.use(session({
+    secret: process.env.SECRET_SESSION,
+    resave: false,
+    saveUninitialized: false
+}));
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+mongoose.connect(process.env.CLUSTER);
 
 const userSchema = new mongoose.Schema({
     email: String,
     password: String
 });
 
-// userSchema.plugin(encrypt, {secret: process.env.SECRET, encryptedFields: ['password']});
+userSchema.plugin(passportLocalMongoose);
+// its gonna hash and salt passwords in database
 
 const user = mongoose.model('userData', userSchema);
 
-app.get('/', (req, res) => {
+passport.use(user.createStrategy());
+
+passport.serializeUser(user.serializeUser());
+passport.deserializeUser(user.deserializeUser());
+
+// if user is authenticated he should directly land on secrets page like twitter 
+app.get('/',(req,res)=>{
+    res.redirect('/secrets');
+});
+
+app.get('/home', (req, res) => {
     res.render('home');
 });
 
@@ -56,49 +67,60 @@ app.get('/register', (req, res) => {
     res.render('register');
 });
 
+app.get('/secrets', (req,res)=>{
+    // we will check if the user is authenticated or not
+    if (req.isAuthenticated()) {
+        res.render('secrets');
+    }
+    else{
+        res.redirect('/home');
+    }
+});
+
 app.post('/register', (req, res) => {
 
-    bcrypt.hash((req.body.password), saltRounds).then(async function(hash) {
-        
-        try {
-            await user.create({
-                email: req.body.username,
-                password: hash
+    user.register({username: req.body.username}, req.body.password, (err, user)=>{
+        if (err) {
+            console.log(err);
+            res.redirect('/register')
+        }else{
+            passport.authenticate('local')(req, res, ()=>{
+                res.redirect('/secrets');
+                // notice we are redirecting instead of rendering as now once user is authenticated we know that he can access the page uninterruptedly.
             });
-            res.render('secrets')
-        } catch (error) {
-            console.log(error);
         }
-
-    }).catch((error)=>{
-        console.log(error);
     });
-
 });
 
 app.post('/login', async (req, res) => {
 
-    const userName = req.body.username;
-    const password = req.body.password;
-    
-    try {
-        const userD = await user.findOne({ email: userName });
-        const match = await bcrypt.compare(password, userD.password);  
-        // bcrypt hashing 
-        
-        if (match) {
-            res.render('secrets');
+    const userL = new user({
+        username: req.body.username,
+        passord: req.body.password
+    });
+
+    req.logIn(userL, (err)=>{
+        if (err) {
+            console.log(err);
         }
         else{
-            res.status(411).json({
-                msg: 'Invalid password'
-            })
+            passport.authenticate('local')(req,res,()=>{
+                res.redirect('/secrets');
+            });
         }
-    } catch (error) {
-        console.log(error);
-    }
+    });
 
 });
+
+app.get('/logout', (req, res) => {
+    req.logout((err) => {
+        if (err) {
+            return next(err);
+        }
+        res.redirect('/');
+    });
+});
+
 
 app.listen(3000, () => {
     console.log('Server is listening on port 3000');
